43dcf90914d4b4ded121a0ffe6fb7704d086ade2
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2022-10-16 04:07:21 -0400

    feature: added vitals
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
+import App from "./App";
+import { DevSupport } from "@react-buddy/ide-toolbox";
+import { ComponentPreviews, useInitial } from "./dev";
+import reportWebVitals from "./reportWebVitals";
+import { reportHandler } from "./reportHandler";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const themes = {
+  light: {
+    foreground: "#000000",
+    background: "#eeeeee",
+  },
+  dark: {
+    foreground: "#ffffff",
+    background: "#222222",
+  },
+};
+
+export const ThemeContext = React.createContext(themes.light);
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
   <React.StrictMode>
-    <App />
+    <ThemeContext.Provider value={themes.dark}>
+      <DevSupport
+        ComponentPreviews={ComponentPreviews}
+        useInitialHook={useInitial}
+      >
+        <App />
+      </DevSupport>
+    </ThemeContext.Provider>
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals(reportHandler);
+reportWebVitals(console.log);
---
+++ src/reportHandler.tsx
@@ -0,0 +1,24 @@
+import { Metric } from "web-vitals";
+
+// event values can only contain integers
+function getEventValueFromMetric(metric: Metric) {
+  if (metric.name === "CLS") {
+    return Math.round(metric.value * 1000);
+  }
+  return Math.round(metric.value);
+}
+
+export function reportHandler(metric: Metric) {
+  const payload = JSON.stringify(metric);
+  navigator.sendBeacon("/analytics", payload);
+}
+
+// function reportHandler(metric: Metric) {
+//     ga('send', 'event', {
+//         eventCategory: 'Web Vitals',
+//         eventAction: metric.name,
+//         eventValue: getEventValueFromMetric(metric),
+//         eventLabel: metric.id,
+//         nonInteraction: true,
+//     });
+// }

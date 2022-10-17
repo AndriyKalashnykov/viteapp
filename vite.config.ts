3904018542a652d49fc63c3f9e96f29e34fc4802
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2022-10-16 20:31:53 -0400

    fix: updated productio build chunk generation
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), chunkSplitPlugin()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    target: "ES2022",
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
     },
   },
 });

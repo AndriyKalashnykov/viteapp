46bf50980ffc2d0336e550c0acbf5c058abcb514
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2022-10-16 21:35:49 -0400

    fix: removed react-buddy
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { DevSupport } from "@react-buddy/ide-toolbox";
-import { ComponentPreviews, useInitial } from "./dev";
 import reportWebVitals from "./reportWebVitals";
 import { reportHandler } from "./reportHandler";
 
@@ -26,12 +25,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeContext.Provider value={themes.dark}>
-      <DevSupport
-        ComponentPreviews={ComponentPreviews}
-        useInitialHook={useInitial}
-      >
-        <App />
-      </DevSupport>
+      <App />
     </ThemeContext.Provider>
   </React.StrictMode>
 );

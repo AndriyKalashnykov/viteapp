87b6387c46c0d779947035c8e272b8ca1943bf35
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2025-08-31 09:49:27 -0400

    fixex: plugins
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
-import { chunkSplitPlugin } from "vite-plugin-chunk-split";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), chunkSplitPlugin()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
@@ -22,6 +21,23 @@ export default defineConfig({
     },
     commonjsOptions: {
       transformMixedEsModules: true,
+    },
+        rollupOptions: {
+      output: {
+        manualChunks: {
+          // vendor split examples
+          react: ["react", "react-dom"],
+          router: ["react-router-dom"],
+        },
+        // or dynamic grouping:
+        // manualChunks(id) {
+        //   if (id.includes("node_modules")) {
+        //     if (id.includes("react")) return "react";
+        //     if (id.includes("react-router")) return "router";
+        //     return "vendor";
+        //   }
+        // }
+      },
     },
   },
 });
